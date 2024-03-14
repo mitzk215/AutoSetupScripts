8095aa77c00bbc40952b5de1da99ca2f67371b77
mitz88 <35113893+mitz88@users.noreply.github.com>
2024-03-14 22:22:52 +0000

    updated if condition

--- Install_venv_django.sh
+++ Install_venv_django.sh
@@ -5,8 +5,8 @@ set -x
 cd ~
 sudo apt update && sudo add-apt-repository universe -y
 
-python3 -m venv
-if [ $? -eq 0 ] ; then
+python3 -m venv dj_pj
+if [ $? -ne 0 ] ; then
   sudo apt install python3.10-venv -y
 else
     echo "venv already installed"
