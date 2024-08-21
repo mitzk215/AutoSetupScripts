fac4b6fa0380dd8fc14fb95d90a29c06cd4ed76e
mitz88 <35113893+mitz88@users.noreply.github.com>
2024-08-21 20:18:07 +0000

    added python3-venv, bucklespring, vim
    added python3-venv, bucklespring, vim

---
+++ new_boot.sh
@@ -0,0 +1,12 @@
+
+
+
+#add universe repository to apt
+sudo add-apt-repository universe -y
+
+#install apps 
+sudo install python3-venv bucklespring vim -y
+
+#start buckle in the background
+buckle -f & 
+
