2327c88396d7e4abf101b0f6e63b823e42eafe62
mitz88 <35113893+mitz88@users.noreply.github.com>
2024-08-23 01:37:28 +0530

    ubuntu settings window shortcut added

--- new_boot.sh
+++ new_boot.sh
@@ -10,3 +10,10 @@ sudo install python3-venv bucklespring vim -y
 #start buckle in the background
 buckle -f & 
 
+# I need to add a shortcut to the ubuntu setting window as well, 
+# but currently i dont know the process to do this using shell scripting 
+# Right now it is a manual setup only like below
+# OPEN UBUNTU SETTING FROM TOP RIGHT PANEL
+# GO TO "KEYBOARD" -> "KEYBOARD SHORTCUTS" -> "View and Customize Shortcuts" -> LAUNCHERS -> SETTINGS
+# Type the desired shortcut. For example: Ctrl + S. Now the shortcut to open "Settings" window is set successfully.
+# Using this chage the timezone, disable the touchpad for typing long thigs.
